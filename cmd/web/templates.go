f2abf072b41c923b5012a735d18662c6c2b419d5
Maksim Kot <kotmaksimarturovich@gmail.com>
2024-08-13 21:06:57 +0300

    Added current year in templateData as common dynamic data, also updated handlers and 'base' template. Improved date readability in 'home' and 'view' pages by using humanDate custom template function

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,13 +3,23 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/Maksim-Kot/Snippetbox/internal/models"
 )
 
 type templateData struct {
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 // Generate templates for every page in ui/html/pages directory
@@ -24,7 +34,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl.html")
 		if err != nil {
 			return nil, err
 		}
