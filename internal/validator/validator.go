7c6a5dec3dc3158efdab10664584603fade1a12a
Maksim Kot <kotmaksimarturovich@gmail.com>
2024-08-19 19:03:22 +0300

    Implemented user signup function. Added 'signup' template and necessary handlers. Updated 'validator' with new functions. Added bcrypt package. Implemented secure storage of user's data (hashed password)

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,16 +1,19 @@
 package validator
 
 import (
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zAZ0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 type Validator struct {
 	FieldErrors map[string]string
 }
 
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) > 0
+	return len(v.FieldErrors) == 0
 }
 
 func (v *Validator) AddFieldError(key, message string) {
@@ -45,4 +48,12 @@ func PermittedInt(value int, permittedValues ...int) bool {
 		}
 	}
 	return false
+}
+
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
 }
